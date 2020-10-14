27b7ab7e64815048a8ed97645b9b89adbe932ef5
Darlin <zkiduxi@gmail.com>
2020-10-14 19:06:55 -0400

    first commit

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atiendeme.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atiendeme.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Appointment([FromBody]Appointment appointment)
+        {
+            var pp = appointment;
+            return Ok(new {
+                userId = 4
+            });
+        }
+
+	[HttpGet]
+        public IActionResult Appointment2()
+        {
+            return Ok(new {
+                userId = 4
+            });
+        }
+    }
+}
